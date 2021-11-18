1b66e47abe67e8fe278ba9781b184884c412509a
OmerHerera <omer.herera@gmail.com>
2021-11-18 12:27:26 +0200

    use secret key for anaytics

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,8 +1,9 @@
 import mixpanel from 'mixpanel-browser';
 // Enabling the debug mode flag is useful during implementation,
 // but it's recommended you remove it for production 
-//process?.env?.MIXPANEL_KEY && mixpanel.init('MIXPANEL_KEY', {debug: true});
-process?.env?.NEXT_PUBLIC_MIXPANEL_KEY && mixpanel.init(process?.env?.NEXT_PUBLIC_MIXPANEL_KEY, {debug: false});
+//process.env.MIXPANEL_KEY && mixpanel.init('MIXPANEL_KEY', {debug: true});
+const secretKey:string = process.env.NEXT_PUBLIC_MIXPANEL_KEY || ""
+secretKey && mixpanel.init(secretKey, { debug: false });
 export function track(eventName: string) {
-  process?.env?.MIXPANEL_KEY && mixpanel.track(eventName);
+  secretKey && mixpanel.track(eventName);
 }
