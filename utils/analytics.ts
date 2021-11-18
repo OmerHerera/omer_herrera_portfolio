89c2e1fc1b30389081d6292d40e9a5be9659b388
OmerHerera <omer.herera@gmail.com>
2021-11-18 11:33:16 +0200

    Change name of Environment Variables

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -2,7 +2,7 @@ import mixpanel from 'mixpanel-browser';
 // Enabling the debug mode flag is useful during implementation,
 // but it's recommended you remove it for production 
 //process?.env?.MIXPANEL_KEY && mixpanel.init('MIXPANEL_KEY', {debug: true});
-process?.env?.MIXPANEL_KEY && mixpanel.init(process?.env?.MIXPANEL_KEY, {debug: false});
+process?.env?.NEXT_PUBLIC_MIXPANEL_KEY && mixpanel.init(process?.env?.NEXT_PUBLIC_MIXPANEL_KEY, {debug: false});
 export function track(eventName: string) {
   process?.env?.MIXPANEL_KEY && mixpanel.track(eventName);
 }
