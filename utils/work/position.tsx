6cdab788674feee0d005b9408b8cbb7f5d3dfa65
OmerHerera <omer.herera@gmail.com>
2021-11-18 15:48:27 +0200

    - Refactor + new position

---
+++ utils/work/position.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import {
+  appsflyerLogo, livePersonLogo, sizmekLogo, bizzaboLogo
+}from '../../assets'
+import styles from './timelinegrid.module.scss'
+export interface PositionProps {
+  company: string;
+  title: string;
+  date: string;
+  description?: React.ReactNode;
+};
+function getLogo(company: string): StaticImageData {
+  switch (company) {
+    case "appsflyer":
+      return appsflyerLogo;
+    case "LivePerson":
+      return livePersonLogo;
+    case "Sizmek":
+      return sizmekLogo;
+    case "Bizzabo":
+      return bizzaboLogo;
+  }
+  return appsflyerLogo;
+}
+export function VPosition({ company, title, date, description }: PositionProps) {
+  return (
+    <div className={styles.timelineGridItemCard}>
+        <div className={styles.cardContent}>
+          <div className={styles.cardContentHeader}>
+            <div className={styles.companyLogo}>
+            <Image
+              alt={`{company} logo.`}
+                src={getLogo(company)}
+                height={50}
+                width={50} />
+            </div>
+            <div className={styles.companyInfo}>
+            <h1 className={styles.companyTitle}>{ company }</h1>
+            <h2 className={styles.cardContentDetails}>{ title }</h2>
+            <h2 className={styles.cardContentDetails}>{ date}</h2></div>
+          </div>
+        <p className={styles.cardContentDescription}>
+          {description}
+        </p>
+        </div>
+      </div>
+  )
+}
--- utils/work/timelinegrid.tsx
+++ utils/work/timelinegrid.tsx
@@ -1,11 +1,5 @@
-import Image from 'next/image'
-import {
-  appsflyerLogo,
-  livePersonLogo,
-  sizmekLogo
-} from '../../assets'
 import styles from './timelinegrid.module.scss'
-
+import { VPosition } from './position'
 export function VTimelineGrid() {
   return (
     <div className={styles.timelineGrid}>
@@ -13,81 +7,64 @@ export function VTimelineGrid() {
         <h1>2021</h1>
         <div className={styles.timelinePoint} />
       </div>
-      <div className={styles.timelineGridItemCard}>
-        <h2 className={styles.hoverTip}>
-          Hover to zoom.
-        </h2>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-              <Image
-                alt="Appsflyer logo."
-                src={appsflyerLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>Appsflyer</h1>
-              <h2 className={styles.cardContentDetails}>Web Tech Lead Platform Team</h2>
-              <h2 className={styles.cardContentDetails}>Feb 2020 - Nov 2021</h2>
-            </div>
-          </div>
-          <p className={styles.cardContentDescription}>
+      
+      {/* BIZZABO */}
+      <VPosition
+        company={"Bizzabo"}
+        title={"Senior Web Engineer Web Foundation Team"}
+        date={"Nov 2021 - Present"}
+        description={
+          <>
+            Building tools and frameworks that are used by front-end developers in the company.
+            Design and implement internal tools and frameworks
+            Build examples and documentation for the tools we are building
+            Educate the developers about the tools
+            Collaborate with different stakeholders in the company (developers, designers, product managers, engineering managers) to help them make the most out of the tools
+          </>
+        }
+      />
+      <div className={styles.timelineGridItemAdjoiningCard} />
+      <div className={styles.timelineGridItemAdjoiningCard}/>
+      {/* APPSFLYER */}
+      <VPosition
+        company={"Appsflyer"}
+        title={"Web Tech Lead Platform Team"}
+        date={"Feb 2020 - Nov 2021"}
+        description={
+          <>
             Part of Platform team that builds, deploy, monitor, and be on call for the platform components and underlying platform infrastructure. 
             <br />* Infrastructure and components used across R&D
             <br />* Monitoring and development of fault-tolerant and highly available infrastructure of projects. Нorizontal Scaling of services
-
             <br />* Documentation, guidance, and escort of developers across the organization
             <br />* Architecture using modern technologies/frameworks like GraphQL, TypeScript, NodeJS, React, webpack, Amazon AWS
             <br />* Modern CI/CD pipeline (Gitlab, Jenkins, Artifactory)
-          </p>
-          
-        </div>
-      </div>
-      <div className={styles.timelineGridItemAdjoiningCard} />
-      <div className={styles.timelineGridItemAdjoiningCard}/>
-      <div className={styles.timelineGridItemCard}>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-              <Image
-                alt="Appsflyer logo."
-                src={appsflyerLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>Appsflyer</h1>
-              <h2 className={styles.cardContentDetails}>Front End Tech Lead Infra Team </h2>
-              <h2 className={styles.cardContentDetails}>Mar 2016 - Feb 2020</h2></div>
-          </div>
-          <p className={styles.cardContentDescription}>
-            Take part in the design and hands-on development of Appsflyer's Front-End infrastructure.
+          </>
+        }
+      />
+      {/* APPSFLYER */}
+      <VPosition
+        company={"Appsflyer"}
+        title={"Front End Tech Lead Infra Team"}
+        date={"Mar 2016 - Feb 2020"}
+        description={
+          <>
+            Take part in the design and hands-on development of Appsflyer's Front-End infrastructure."
             <br />* Modules and components used across R&D
             <br />* Micro-services architecture using modern technologies/frameworks like NodeJS, React, SASS, webpack, Amazon AWS
             <br />*Modern CI/CD pipeline (Gitlab, Jenkins, Artifactory)
             <br />*Documentation, guidance and escort of developers across the organization
-          
-          </p>
-        </div>
-      </div>
-      <div className={styles.timelineGridItemCard}>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-              <Image
-                alt="LivePerson logo."
-                src={livePersonLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>LivePerson</h1>
-              <h2 className={styles.cardContentDetails}>Front End Engineer Infrastructure Team </h2>
-              <h2 className={styles.cardContentDetails}>Jan 2013 - Feb 2016</h2>
-            </div>
-          </div>
-          <p className={styles.cardContentDescription}>
+          </>
+        }
+      />
+      <div className={styles.timelineGridItemAdjoiningCard} />
+      <div className={styles.timelineGridItemAdjoiningCard}/>
+      {/* LIVEPERSON */}
+      <VPosition
+        company={"LivePerson"}
+        title={"Front End Engineer Infrastructure Team"}
+        date={"Jan 2013 - Feb 2016"}
+        description={
+          <>
             Take part in the design and hands-on development of LivePerson's Front-End infrastructure.
             Responsibility for all front-end development, building both on native browser capabilities and modern client side libraries
             Researching and designing solutions for new features.
@@ -95,88 +72,49 @@ export function VTimelineGrid() {
             Web development in Client Side deployment and coding in high level JS, CSS, HTML.
             An excellent cross-browser compatibility understanding (Web standards/XHTML/CSS/Javascript).
             Knowledge in Backbone, MarionetteJS, jQuery, AJAX, GruntJS, NodeJS and Sass.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.timelineGridItemAdjoiningCard} />
-      <div className={styles.timelineGridItemAdjoiningCard}/>
-      <div className={styles.timelineGridItemCard}>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-            <Image
-                alt="LivePerson logo."
-                src={livePersonLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>LivePerson</h1>
-              <h2 className={styles.cardContentDetails}>Software Engineer</h2>
-              <h2 className={styles.cardContentDetails}>May 2012 – Jan 2013</h2></div>
-          </div>
-          <p className={styles.cardContentDescription}>
+          </>
+        }
+      />
+      {/* LIVEPERSON */}
+      <VPosition
+        company={"LivePerson"}
+        title={"Software Engineer"}
+        date={"May 2012 – Jan 2013"}
+        description={
+          <>
             Part of the team that developed the new LivePerson chat engagements (buttons & invitations) application:
             Developing both server side and client side of the engagements configuration in the application back-end.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.timelineGridItemCard}>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-              <Image
-                alt="Sizmek logo."
-                src={sizmekLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>MediaMind</h1>
-              <h2 className={styles.cardContentDetails}>Web Software Engineer</h2>
-              <h2 className={styles.cardContentDetails}>Feb 2007 - Jun 2012</h2>
-            </div>
-          </div>
-          <p className={styles.cardContentDescription}>
+          </>
+        }
+      />
+      <div className={styles.timelineGridItemAdjoiningCard} />
+      <div className={styles.timelineGridItemAdjoiningCard}/>
+      {/* SIZMEK */}
+      <VPosition
+        company={"Sizmek"}
+        title={"Web Software Engineer"}
+        date={"Feb 2007 - Jun 2012"}
+        description={
+          <>
             Development in the Web Client infrastructure of the served ads. Written in core JavaScript.
             The role involved both logic and visual (HTML DOM manipulation) challenges, knowledge of Internet technologies, development in high level JavaScript, cross browsers knowledge, ASP/.NET and other server side scripting languages.
             Responsible for defining, designing and building the client infrastructure of the company.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.timelineGridItemAdjoiningCard} />
-      <div className={styles.timelineGridItemAdjoiningCard}/>
-      
-
-
-      <div className={styles.timelineGridItemCard}>
-        <div className={styles.cardContent}>
-          <div className={styles.cardContentHeader}>
-            <div className={styles.companyLogo}>
-              <Image
-                alt="Sizmek logo."
-                src={sizmekLogo}
-                height={50}
-                width={50} />
-            </div>
-            <div className={styles.companyInfo}>
-              <h1 className={styles.companyTitle}>MediaMind</h1>
-              <h2 className={styles.cardContentDetails}>Software Engineer</h2>
-              <h2 className={styles.cardContentDetails}>Jan 2005 - Feb 2007</h2>
-            </div>
-          </div>
-          <p className={styles.cardContentDescription}>
+          </>
+        }
+      />
+      {/* SIZMEK */}
+      <VPosition
+        company={"Sizmek"}
+        title={"Software Engineer"}
+        date={"Feb 2005 - Jun 2007"}
+        description={
+          <>
             Developing IIS server extension(ISAPI), server infrastructure (C++) ,windows services (C#), low latency infrastructure and ad serving server.
             Implementation of methodologies and standards.
             Multithreading and interprocess communications
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.timelineGridItemAdjoiningCard}/>
+          </>
+        }
+      />
       <div className={styles.timelineGridItem}/>
       <div className={styles.timelineGridItemWide}>
         <div className={styles.timelinePoint}/>
